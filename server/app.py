845b41b566a91b607b2fe40c856df46439856049
Andy Patari <andy.patari@gmail.com>
2023-11-10 10:04:37 -0500

    add signup and login routes to api

--- server/app.py
+++ server/app.py
@@ -31,8 +31,26 @@ class Signup(Resource):
         
         except Exception as err:
             return {"errors": [repr(err)]}, 422
+        
+class Login(Resource):
 
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
 
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            response_body = {
+                "id": user.id,
+                "username": user.username,
+            }
+            return response_body, 200
+        else:
+            return {"errors": ["Error: invalid username and or password"]}, 401
+
+api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(Signup, '/signup', endpoint='signup')
 
 if __name__ == '__main__':
