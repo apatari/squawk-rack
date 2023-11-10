4f1f848cacd9d1b96f78010d79396014262e4c17
Andy Patari <andy.patari@gmail.com>
2023-11-10 10:31:42 -0500

    add login page styling

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,33 +1,36 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Row, Col } from "react-bootstrap";
 
 function Login(){
     return (
         <div>
 
-            <h1>
-            Login
-            </h1>
-            <Form>
-                <Form.Group controlId="formBasicUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="username" placeholder="Enter username" />
-                {/* <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text> */}
-                </Form.Group>
             
-                <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group controlId="formBasicCheckbox">
+            <Col lg="5" className="mx-auto">
+                <h1 className="m-5">
+                Login
+                </h1>    
+                <Form className="m-5">
+                    <Form.Group className="m-3" controlId="formBasicUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="username" placeholder="Enter username" />
+                    {/* <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text> */}
+                    </Form.Group>
                 
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                Submit
-                </Button>
-            </Form>
+                    <Form.Group className="m-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                    
+                    </Form.Group>
+                    <Button className="m-3" variant="primary" type="submit">
+                    Submit
+                    </Button>
+                </Form>
+            </Col>
         </div>
         
     )
