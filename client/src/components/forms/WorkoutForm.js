97acd00d71f1c212d5a116ca905cbefce3cb5d10
Andy Patari <andy.patari@gmail.com>
2023-11-21 10:26:45 -0500

    workouts ost route working

--- client/src/components/forms/WorkoutForm.js
+++ client/src/components/forms/WorkoutForm.js
@@ -1,19 +1,58 @@
 import React, { useState } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import ExerciseForm from "./ExerciseForm";
 import ExerciseList from "../cards/ExerciseList";
+import { useFormik } from "formik"
+import * as yup from "yup"
 
 
 function WorkoutForm({ user }) {
 
+    const history = useHistory()
+
     const [exercises, setExercises] = useState([])
+
+    const formSchema = yup.object().shape({
+        name: yup.string()
+            .required("Must name workout")
+            .max(40, "Name must be 40 characters or fewer"),
+        details:  yup.string()
+            .required("Please provide details/description")
+            .max(1000, "Description must be 1000 characters or fewer"),
+        
+    })
+
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            detailks: ""
+        },
+        validationSchema: formSchema,
+        onSubmit: (values) => { 
+
+            fetch("/api/workouts", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({...values, "user_id":user.id, "exercises": exercises}),
+            }).then((res) => {
+              if (res.status == 201) {
+                    return res.json()
+                    .then(workout => {history.push(`/workouts/${workout.id}`)})
+                  ;
+              }
+            });
+          }
+    })
 
     return (
         <div>
             <Col lg="6" className="mx-auto">
                 
                      
-                <Form className="mt-5"  >
+                <Form className="mt-5" onSubmit={formik.handleSubmit} >
                     <Col md="7">
                         <Form.Group className="mb-4 " >
                             <Form.Label className="fs-4" >Workout Name</Form.Label>
--- server/app.py
+++ server/app.py
@@ -99,6 +99,7 @@ class WorkoutIndex(Resource):
     def post(self):
 
         json = request.get_json()
+        exercises = json['exercises']
 
         try:
             new_workout = Workout(
@@ -109,8 +110,25 @@ class WorkoutIndex(Resource):
             db.session.add(new_workout)
             db.session.commit()
 
-            return new_workout.to_dict(), 201
+            try:
+                for exercise in exercises:
+                    new_exercise = Exercise(
+                                            name=exercise['name'],
+                                            sets=exercise['sets'],
+                                            reps=exercise['reps'],
+                                            order_number=exercise['order_number'],
+                                            workout_id=new_workout.id,
+                                            )
+                    db.session.add(new_exercise)
+                    db.session.commit()
+                    
+
+                return new_workout.to_dict(), 201
         
+            except Exception as err:
+
+                return {"errors": [str(err)]}, 422
+
         except Exception as err:
             return {"errors": [str(err)]}, 422
 
@@ -126,6 +144,21 @@ class WorkoutByID(Resource):
             return workout, 200
         else:
             return {"error": "workout not found"}, 404
+        
+    def post(self):
+
+        json = request.get_json()
+        
+
+        try:
+            new_workout = Workout(user_id=json['user_id'], name=json['name'], details=json['details'])
+            
+            db.session.add(new_workout)
+            db.session.commit()
+        except Exception as err:
+            return {"errors": [str(err)]}, 422
+
+        
 
 class FavoriteIndex(Resource):
 
@@ -171,31 +204,31 @@ class ReviewIndex(Resource):
         except Exception as err:
             return {"errors": [str(err)]}, 422
 
-class ExerciseIndex(Resource):
+# class ExerciseIndex(Resource):
 
-    def post(self):
+#     def post(self):
 
-        json = request.get_json()
-        exercises = json['exercises']
-        result=[]
+#         json = request.get_json()
+#         exercises = json['exercises']
+#         result=[]
 
-        try:
-            for exercise in exercises:
-                new_exercise = Exercise(
-                                        name=exercise['name'],
-                                        sets=exercise['sets'],
-                                        reps=exercise['reps'],
-                                        order_number=exercise['order_number'],
-                                        workout_id=exercise['workout_id'],
-                                        )
-                db.session.add(new_exercise)
-                db.session.commit()
-                result.append(new_exercise.to_dict())
+        # try:
+        #     for exercise in exercises:
+        #         new_exercise = Exercise(
+        #                                 name=exercise['name'],
+        #                                 sets=exercise['sets'],
+        #                                 reps=exercise['reps'],
+        #                                 order_number=exercise['order_number'],
+        #                                 workout_id=exercise['workout_id'],
+        #                                 )
+        #         db.session.add(new_exercise)
+        #         db.session.commit()
+        #         result.append(new_exercise.to_dict())
 
-            return result, 201
+        #     return result, 201
         
-        except Exception as err:
-            return {"errors": [str(err)]}, 422
+        # except Exception as err:
+        #     return {"errors": [str(err)]}, 422
 
 
 @app.route('/')
@@ -204,7 +237,7 @@ def index(id=0):
     return render_template("index.html")
 
 
-api.add_resource(ExerciseIndex, '/api/exercises', endpoint='exercises')
+# api.add_resource(ExerciseIndex, '/api/exercises', endpoint='exercises')
 api.add_resource(ReviewIndex, '/api/reviews', endpoint='reviews')
 api.add_resource(FavoriteIndex, '/api/favorites', endpoint='favorites')
 api.add_resource(WorkoutByID, '/api/workouts/<int:id>', endpoint='workout_by_id')
