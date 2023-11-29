5b0e07d21467b584e315bacfe809a870f26614b8
Andy Patari <andy.patari@gmail.com>
2023-11-29 08:02:13 -0500

    remove bootstrap css line

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Header from "./Header";
 import "bootswatch/dist/morph/bootstrap.min.css";
-// import 'bootstrap/dist/css/bootstrap.min.css'
+
 import MyWorkouts from "./MyWorkouts";
 import Explore from "./Explore";
 import Create from "./Create";
